0b95684d94b04a9a0deb5844b33bf37fd5ca7415
Chepe Garcia riboldi <chepegarciariboldi@Chepes-MacBook-Pro.local>
2023-12-14 13:05:47 -0300

    fix issues

--- src/endpoints/libro.router.ts
+++ src/endpoints/libro.router.ts
@@ -5,7 +5,7 @@ import controladorLibro from '../controladores/controladorLibro';
 const router = express.Router();
 
 router.get('/', controladorLibro.getAllLibros);
-router.get('/:id', controladorLibro.getLibroByTitulo);
+router.get('/:id', controladorLibro.getLibroById);
 router.post('/', controladorLibro.createLibro);
 router.put('/:id', controladorLibro.updateLibro);
 router.delete('/:id', controladorLibro.deleteLibro);
